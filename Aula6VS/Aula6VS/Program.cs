4124bd60f2640dfc281bc8f472de674a3c455b61
Thamyres <thamy20016@gmail.com>
2022-04-07 15:46:06 -0300

    Exercicios CSharp- aula7

---
+++ Aula6VS/Aula6VS/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Aula6VS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int numero;
+
+            Console.WriteLine("Tabuada");
+            Console.WriteLine("Insira o numero que voçê quer multiplicar:");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            for (int x = 1; x <= 10; x++)
+            {
+                int resultado = numero * x;
+                Console.WriteLine($"{numero} X {x} = {resultado}");
+            }
+        }
+
+    }
+}
+
+
