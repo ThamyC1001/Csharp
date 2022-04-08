27c8c16b85bb78e63e51c3fe235a872d34f79b80
Thamyres <thamy20016@gmail.com>
2022-04-08 18:14:05 -0300

    Exercicio8 CSharp

---
+++ Galinha.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula8VS.src
+{
+    internal class Galinha
+    {
+
+        public string Nome { get; set; }
+        public string Cor { get; set; }
+        public string Alimento { get; set; }
+        public string Classes { get; set; }
+        public Galinha(string _nome, string _cor, string _alimento, string _classes)
+        {
+            Nome = _nome;
+            Cor = _cor;
+            Alimento = _alimento;
+            Classes = _classes;
+        }
+        public void botarovo()
+        {
+            Console.WriteLine("rosangela esta chocando ovo ...");
+        }
+        public void comer(string comida)
+        {
+            Console.WriteLine($"Eu isabelly estou comendo {comida}");
+        }
+
+        public void dormir()
+        {
+            Console.WriteLine($"claudia esta dormindo:"+ "zzzzzzzzzz..");
+        }
+       
+    }
+}
+
+    
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Aula8VS.src;
+using System;
+
+namespace Aula8VS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Galinhazinha");
+            Galinha isabelly = new Galinha("isabelly", "rosa", "milho", "aves");
+            Galinha claudia = new Galinha(" claudia", "branca", "grilo", "aves");
+            Galinha rosangela = new Galinha("rosangela", "marrom", "sementes", "aves");
+
+            Console.WriteLine($"galinha 1 é a {isabelly.Nome} de cor {isabelly.Cor} e Classes {isabelly.Classes}");
+            Console.WriteLine($"galinha 2 é a {claudia.Nome} de cor {claudia.Cor} e Classes {claudia.Classes}");
+            Console.WriteLine($"galinha 3 é a {rosangela.Nome} de cor {rosangela.Cor} e Classes {rosangela.Classes}");
+
+            isabelly.comer("grilos");
+            claudia.dormir();
+            rosangela.botarovo();
+            Console.WriteLine();
+        }
+
+
+    }
+}
